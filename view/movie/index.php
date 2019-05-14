9eef7a7557ac7f30d2be9d91b6f2d47da3aea901
Peo Eriksson <p-o.eriksson@hotmail.com>
2019-05-14 14:28:16 +0200

    Movie route integrated into redovisa.

---
+++ view/movie/index.php
@@ -0,0 +1,31 @@
+<?php
+namespace Anax\View;
+
+/**
+ * Render the view to show all movies
+ */
+
+if (!$res) {
+    return;
+}
+
+
+?><table>
+    <tr class="first">
+        <th>Rad</th>
+        <th>Id</th>
+        <th>Bild</th>
+        <th>Titel</th>
+        <th>År</th>
+    </tr>
+<?php $id = -1; foreach ($res as $row) :
+    $id++; ?>
+    <tr>
+        <td><?= $id ?></td>
+        <td><?= $row->id ?></td>
+        <td><img class="thumb" src="<?= $row->image ?>"></td>
+        <td><?= $row->title ?></td>
+        <td><?= $row->year ?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
